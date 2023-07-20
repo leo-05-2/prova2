bf01a2e68f78a35f4ce253d86f0a31369c44d45f
leoke <leoviti9@gmail.com>
2023-07-20 16:30:34 +0200

    terzo commit

---
+++ cmake-build-debug/CMakeFiles/prova2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prova2.dir/main.cpp.o"
+  "CMakeFiles/prova2.dir/main.cpp.o.d"
+  "CMakeFiles/prova2.dir/test.cpp.o"
+  "CMakeFiles/prova2.dir/test.cpp.o.d"
+  "prova2"
+  "prova2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prova2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- test.h
+++ test.h
@@ -10,6 +10,7 @@ class test {
 public:
     int c;
 
+
 };
 
 
